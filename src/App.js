a8de8516c25d2c0236a9505f2cbbea97dd06d2b2
yigith <yigith1@gmail.com>
2023-03-03 15:57:39 +0300

    bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Container, Form, ListGroup } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className='mt-3'>To-Do List</h1>
+        <Form className='d-flex mt-3'>
+          <Form.Control className="me-2" />
+          <Button>
+            <FontAwesomeIcon icon={faPlus} />
+          </Button>
+        </Form>
+        <ListGroup className='mt-3'>
+          <ListGroup.Item>
+            Güneşe çık
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Araya çık
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Ehlikeyfe git
+          </ListGroup.Item>
+        </ListGroup>
+      </Container>
     </div>
   );
 }
